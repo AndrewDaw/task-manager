b603cb2ac6d333089b6d8d65c9350cf49d4e24e2
AndrewDaw <andrew.daw1995@gmail.com>
2019-08-19 20:37:23 +1200

    Added routes for creating, reading and updating user and task records

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -6,17 +6,6 @@ const dbName = 'task-manager-api'
 
 mongoose.connect(connectionURL+'/'+dbName ,{
     useNewUrlParser: true,
-    useCreateIndex: true
-})
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        trim: true,
-        required: true
-    }, 
-    completed: {
-        type: Boolean,
-        default: false
-    }
+    useCreateIndex: true,
+    useFindAndModify: false
 })
