9d1a5b12f2a5a5785fb578dd7cd58241fec743ab
AndrewDaw <andrew.daw1995@gmail.com>
2019-08-29 18:32:34 +1200

    Now serves up profile images and stores them in database

--- src/routers/user.js
+++ src/routers/user.js
@@ -106,12 +106,47 @@ router.delete('/users/me', auth, async (req, res) => {
     }
 })
 
+//for uploading files
 const upload = multer({
-    dest: 'avatars'
+    limits: {
+        fileSize: 1000000
+    },
+    fileFilter(req, file, cb) {
+        if(!file.originalname.match(/\.(jpg|jpeg|png|bmp)$/)) {
+            return cb(new Error('Please upload an image'))
+        }
+        cb(undefined, true)
+    }
 })
 
-router.post('/users/me/avatar', upload.single('avatar'), async(req,res) => {
+router.post('/users/me/avatar', auth, upload.single('avatar'), async (req,res) => {
+    req.user.avatar = req.file.buffer
+    await req.user.save()
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({error: error.message})
+})
+
+router.delete('/users/me/avatar', auth, async (req,res) => {
+    req.user.avatar = undefined
+    await req.user.save
+    res.send()
+})
+
+router.get('/users/:id/avatar', async(req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        if (!user || !user.avatar) {
+            console.log('grrr')
+            throw new Error()
+        }
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+
+    } catch {
+        res.status(404).send()
+        console.log('Cant find image')
+    }
 })
 
 
